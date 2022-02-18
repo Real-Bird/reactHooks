4afba81664521acc60456e628f09e474bb8fa0c6
Real_Bird <83404864+Real-Bird@users.noreply.github.com>
2022-02-19 08:40:16 +0900

    22-02-19

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import "./styles.css";
+import React, { useState } from "react";
 
-export default function App() {
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    setValue(value);
+  };
+  let willUpdate = true;
+  if (typeof validator === "function") {
+    willUpdate = validator(value);
+  }
+  if (willUpdate) {
+    setValue(value);
+  }
+  return { value, onChange };
+};
+
+const App = () => {
+  const [item, setItem] = useState(0);
+  const incremetItem = () => setItem(item + 1);
+  const decremetItem = () => setItem(item - 1);
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("mr. ", maxLen);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h2>{item}</h2>
+      <button onClick={incremetItem}>incremetItem</button>
+      <button onClick={decremetItem}>decremetItem</button>
+      <div>
+        <input placeholder="Name" {...name} />
+      </div>
     </div>
   );
-}
+};
+
+export default App;
