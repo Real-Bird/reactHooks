6f650490f613969750eab20fc9bfc4e3964d1034
Real_Bird <83404864+Real-Bird@users.noreply.github.com>
2022-02-19 09:24:12 +0900

    22-02-19 useState hooks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import React, { useState } from "react";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event;
-    setValue(value);
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
   };
-  let willUpdate = true;
-  if (typeof validator === "function") {
-    willUpdate = validator(value);
-  }
-  if (willUpdate) {
-    setValue(value);
-  }
-  return { value, onChange };
 };
 
 const App = () => {
-  const [item, setItem] = useState(0);
-  const incremetItem = () => setItem(item + 1);
-  const decremetItem = () => setItem(item - 1);
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("mr. ", maxLen);
-
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div>
-      <h2>{item}</h2>
-      <button onClick={incremetItem}>incremetItem</button>
-      <button onClick={decremetItem}>decremetItem</button>
-      <div>
-        <input placeholder="Name" {...name} />
-      </div>
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
---
+++ src/useInput.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+  const [item, setItem] = useState(0);
+  const incremetItem = () => setItem(item + 1);
+  const decremetItem = () => setItem(item - 1);
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("mr. ", maxLen);
+
+  return (
+    <div>
+      <h2>{item}</h2>
+      <button onClick={incremetItem}>incremetItem</button>
+      <button onClick={decremetItem}>decremetItem</button>
+      <div>
+        <input placeholder="Name" {...name} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
