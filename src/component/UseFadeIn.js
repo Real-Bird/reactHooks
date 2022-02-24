29a2eed06a8ccb5b39b51f99f538407017286f9d
Real_Bird <83404864+Real-Bird@users.noreply.github.com>
2022-02-24 20:20:25 +0900

    22-02-24

--- src/component/UseFadeIn.js
+++ src/component/UseFadeIn.js
@@ -18,10 +18,12 @@ const useFadeIn = (duration = 1, delay = 0) => {
 const UseFadeIn = () => {
   const fadeInH1 = useFadeIn(1, 2);
   const fadeInP = useFadeIn(5, 5);
+  const reload = () => location.reload();
   return (
     <div>
       <h1 {...fadeInH1}>Hello, World!!!</h1>
       <p {...fadeInP}>lorem ipsum</p>
+      <button onClick={reload}>새로고침</button>
     </div>
   );
 };
